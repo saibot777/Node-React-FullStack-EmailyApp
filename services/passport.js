67927f6f5d1315f4b0ca7c11481d1da6960d26b6
saibot777 <stefantrajkovic777@gmail.com>
2017-09-04 23:57:39 +0200

    initial commit

---
+++ services/passport.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User  = mongoose.model('users');
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+            new User({ googleId: profile.id }).then(existingUser => {
+                if (existingUser) {
+                    // we already hyave a record with given id
+                } else {
+                    // we don't have a user record with this id, make a new one
+                    new User({googleId: profile.id}).save();
+                }
+            });
+        }
+    )
+);
